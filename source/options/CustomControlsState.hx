891aa31402234ea011ce9252cf98320a20487709
Sirox <71220271+Sirox228@users.noreply.github.com>
2022-09-04 23:53:44 +0300

    Update CustomControlsState.hx

--- source/options/CustomControlsState.hx
+++ source/options/CustomControlsState.hx
@@ -112,7 +112,7 @@ class CustomControlsState extends MusicBeatSubstate
 
 		var savebutton = new FlxUIButton((exitbutton.x + exitbutton.width + 25),25,"exit and save",() -> {
 			save();
-			FlxG.switchState(new OptionsMenu());
+			FlxG.switchState(new OptionsDirect());
 		});
 		savebutton.resize(250,50);
 		savebutton.setLabelFormat("VCR OSD Mono",24,FlxColor.BLACK,"center");
@@ -167,7 +167,7 @@ class CustomControlsState extends MusicBeatSubstate
 		var androidback:Bool = false;
 		#end
 		if (exitbutton.justReleased || androidback){
-			FlxG.switchState(new OptionsMenu());
+			FlxG.switchState(new OptionsDirect());
 		}
 		
 		for (touch in FlxG.touches.list){
