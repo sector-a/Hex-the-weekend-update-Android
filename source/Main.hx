66035bf18c6dfa9429e7b1f5f4d1ecb191579635
Sirox <71220271+Sirox228@users.noreply.github.com>
2022-09-04 16:39:47 +0300

    idk

--- source/Main.hx
+++ source/Main.hx
@@ -103,7 +103,7 @@ class Main extends Sprite
 		bitmapFPS = ImageOutline.renderImage(fpsCounter, 1, 0x000000, true);
 		bitmapFPS.smoothing = true;
 
-		game = new FlxGame(gameWidth, gameHeight, initialState, Std.int(zoom), framerate, framerate, true, false);
+		//game = new FlxGame(gameWidth, gameHeight, initialState, Std.int(zoom), framerate, framerate, true, false);
                 game = new FlxGame(gamewidth, gameheight, initialState, zoom, 60, 60, true);
 		addChild(game);
 
